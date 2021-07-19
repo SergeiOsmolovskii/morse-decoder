55a6fa6334777d119dd3e991f41addeb17d089cc
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2021-07-19 23:13:34 +0300

    feat: add Mose decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let symbols = [];
+    let letters = [];
+    let letter = [];
+
+    for(let i = 0; i< expr.toString().length; i += 10 ) {
+        symbols.push(expr.toString().slice(i, i + 10));
+    }
+
+    for(let i = 0; i < symbols.length; i++ ) {
+        if(symbols[i] == '**********') letter.push(' ');
+        
+        for(let j = 0; j < symbols[i].length; j += 2 ) {
+            if(symbols[i].slice(j, j + 2) == 00) letter.push('');
+            if(symbols[i].slice(j, j + 2) == 10) letter.push('.');
+            if(symbols[i].slice(j, j + 2) == 11) letter.push('-');
+        }
+        letters.push(letter);
+        letter = [];
+    }
+
+    for(let i = 0; i < letters.length; i++ ) {
+        letters[i] = letters[i].join('');
+    }
+
+    for(let i = 0; i < letters.length; i++ ) {
+        for(let key in MORSE_TABLE) {
+            if(key == letters[i]) letters[i] = MORSE_TABLE[letters[i]] ;
+        }
+    }
+
+    return letters.join('');
 }
 
 module.exports = {
